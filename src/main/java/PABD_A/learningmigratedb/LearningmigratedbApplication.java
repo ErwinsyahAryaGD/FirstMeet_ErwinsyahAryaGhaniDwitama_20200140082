303fb8b968c519895c221a7b773b9363eddfd98e
USER <USER@DESKTOP-30P3A3H>
2022-10-18 15:54:00 +0700

    Commit pertama

---
+++ src/main/java/PABD_A/learningmigratedb/LearningmigratedbApplication.java
@@ -0,0 +1,13 @@
+package PABD_A.learningmigratedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningmigratedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearningmigratedbApplication.class, args);
+	}
+
+}
